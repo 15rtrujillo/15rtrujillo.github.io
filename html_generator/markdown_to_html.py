b9d0a5a57bb508fe1278fb15ca86f3f5a48d4a21
Rrrrry123 <ryantrujillo@comcast.net>
2024-12-12 12:14:55 -0700

    Testing. Need to redo some regexes.

--- html_generator/markdown_to_html.py
+++ html_generator/markdown_to_html.py
@@ -2,14 +2,28 @@ import re
 
 
 BOLD = r"(?<!\\)\*\*([^\\]+?(?:\\.[^\\]*?)*?)\*\*"
+RE_BOLD = re.compile(BOLD)
+UNDERLINED = r"(?<!\\)__([^\\]+?(?:\\.[^\\]*?)*?)__"
+RE_UNDERLINED = re.compile(UNDERLINED)
 ITALICS = r"(?<!\\)\*([^*\\]+(?:\\.[^*\\]*)*?)\*"
+RE_ITALICS = re.compile(ITALICS)
 ORDERED_LIST = r"^[0-9]+\.\s(.+)$"
+RE_ORDERED_LIST = re.compile(ORDERED_LIST)
 UNORDERED_LIST = r"^-\s(.+)$"
+RE_UNORDERED_LIST = re.compile(UNORDERED_LIST)
 LINKS = r"\[(.+?)\]\((.+?)\)"
+RE_LINKS = re.compile(LINKS)
+
+
+def parse_text_decoration(text: str, tag: str, pattern: re.Pattern[str]) -> str:
+    matches = re.finditer(pattern, text)
+    for found in matches:
+        print(found.string, found.group(1))
 
 
 def parse_markdown_to_html(text: str) -> str:
-    pass
+    for line in text.split("\n"):
+        parse_text_decoration(line, "em", RE_ITALICS)
 
 
 if __name__ == "__main__":
